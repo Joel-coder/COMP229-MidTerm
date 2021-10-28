79991dee8ddc89839a425dfbd5d547be8c3a3108
Joel Vargas <joelvargasapolinario@gmail.com>
2021-10-27 21:12:39 -0400

    DB URI

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  URI: "mongodb+srv://jvargas:cfSNBxtflvARkF8U@mongodbserver.36qg1.mongodb.net/contacts_list?retryWrites=true&w=majority",
 };
